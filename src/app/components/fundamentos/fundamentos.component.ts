e075e8a05acddf4ba28185e2259134f25dab18bb
NataRey <lenarero@gmail.com>
2024-05-18 21:09:08 -0500

    formularios reactivos y fomularios template-driven

--- src/app/components/fundamentos/fundamentos.component.ts
+++ src/app/components/fundamentos/fundamentos.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PadreComponent } from '../padre/padre.component';
-
+// del modulo de angular forms voy a importar 
+import { FormsModule, ReactiveFormsModule, FormGroup,FormControl} from '@angular/forms';
 @Component({
   selector: 'app-fundamentos',
   standalone: true,
-  imports: [PadreComponent],
+  //nunca olvidar activar mis importaciones  en el caso de FormGroup,FormControl no es necesario activarlas por que el ReactiveFormsModule me trae por debajo estas otras dos 
+  imports: [PadreComponent,FormsModule,ReactiveFormsModule ],
   templateUrl: './fundamentos.component.html',
   styleUrl: './fundamentos.component.css'
 })
@@ -62,4 +64,29 @@ export class FundamentosComponent {
     console.log("up");
     this.colorFondo = "cfAzul";
   }
+  // clase 17 de mayo
+  //estoy utilizando un tipo de formulario y para usarlo debo importar un modulo de forms  que me permite usar una directiva de angular que se llama ngModel y lo que hace la directiva es recibir el dato y mostrala en mi html
+  //voy a crear una propiedad que reciba el framework favorito inicialmente con un string vacio
+  frameworkFavorito: string = "";
+  // lo que se pretende es que cuando se de clic en el boton me traiga el contenido del framework que ingreso el usuario 
+  mostrarFrameworkFavorito(){
+    console.log("mostrar:", this.frameworkFavorito);
+    // en este punto no me sale ninguna informacion por que no lo he enlazado
+    //para enlazarlo voy al html a mi input 
+  }
+
+  //creo esa propiedad que va a ser una nueva instancia de ese formgrup pero para que reciba los datos agrupados debo decirle a formGroup que use el FormControl
+  Formulariocredenciales = new FormGroup({
+    //pero primero creo las propiedades con el usuario y contrasena diciendo que usuario tiene como instancia un nuevo formcontrol con un string vacio
+    usuario: new FormControl(''),
+    contrasenia: new FormControl('')
+    // debemos crear un metodo para que cuando yo pulse el boton pueda gestionar el manejo de los datos 
+
+  });
+  //creamos el metodo manejarenvio()
+  manejarEnvio(){
+    //que por el momento va a mostrar las credenciales que digito el usuario al dejarlo de esta forma console.log("credenciales", this.Formulariocredenciales); me va a mostrar un formGroup que traer varios datos de la libreria entre esos las credenciales de usuarios y contrasena pero si yo quiero ver solo estos datos al final agrego un .value para que solo me traiga los datos que tengo en mi input quedaria de esta forma console.log("credenciales", this.Formulariocredenciales.value);
+    console.log("credenciales", this.Formulariocredenciales.value);
+  }
+  
 }
